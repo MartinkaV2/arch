d3c2c58037cb0acf1d05fba0838711a2b5b3ad27
MartinkaV2 <70255079+MartinkaV2@users.noreply.github.com>
2024-12-28 20:36:35 +0100

    new and old package removed

--- 03programs.sh
+++ 03programs.sh
@@ -1,39 +1,19 @@
 #!/bin/sh
 
+#Screenshot
+sudo pacman -S gnome-screenshot --noconfirm
+
 #Flatseal
 flatpak install flathub com.github.tchx84.Flatseal-- noconfirm
 
-#Mangohud
-mkdir mangohud
-cd mangohud/
-git clone https://aur.archlinux.org/mangohud-git.git
-cd mangohud-git/
-makepkg -si --noconfirm
-cd ..
-rm -rf mangohud-git
-
-#lib32-mangohud
-cd mangohud/
-git clone https://aur.archlinux.org/lib32-mangohud-git.git
-cd lib32-mangohud-git
-makepkg -si --noconfirm
-cd ..
-rm -rf lib32-mangohud-git
-cd ..
-rm -rf mangohud
+#Mangohud & GOverlay
+sudo pacman -S mangohud lib32-mangohud goverlay --noconfirm
 
 #Vencord (Discord alternative with plugins)
 sh -c "$(curl -sS https://raw.githubusercontent.com/Vendicated/VencordInstaller/main/install.sh)"
 
-#GOverlay
-git clone https://aur.archlinux.org/goverlay-git.git
-cd goverlay-git/
-makepkg -si
-cd ..
-rm -rf goverlay-git
-
 #Gamemode & Gamescope
-sudo pacman -Sy gamemode gamescope --noconfirm
+sudo pacman -S gamemode gamescope --noconfirm
 
 #Papirus icon pack
 sudo pacman -S papirus-icon-theme --noconfirm
@@ -42,24 +22,60 @@ sudo pacman -S papirus-icon-theme --noconfirm
 flatpak install flathub io.freetubeapp.FreeTube --noconfirm
 
 #Lutris
-sudo pacman -Sy lutris --noconfirm
+sudo pacman -S lutris --noconfirm
 
-#Steam
-sudo pacman -Sy steam --noconfirm
+#Steam Nvidia kártyákhoz
+sudo pacman -S steam nvidia-utils lib32-nvidia-utils --noconfirm
+sudo chmod +x update-proton-ge
+./update-proton.sh
 
 #OBS
 flatpak install flathub com.obsproject.Studio
 
 #Heroic
-git clone https://aur.archlinux.org/heroic-games-launcher-bin.git
-cd heroic-ganes-launcher-bin/
-makepkg -si --noconfirm
-cd ..
-rm -rf heroic-games-launcher
+yay -S heroic-games-launcher-bin --noconfirm
 
 #Brave
-git clone https://aur.archlinux.org/brave-bin.git
-cd brave-bin/
-makepkg -si --noconfirm
-cd ..
-rm -rf brave-bin
+sudo pacman -S brave-bin --noconfirm
+
+#P7Zip - tömörítő program
+yay -S p7zip-full-bin --noconfirm
+
+#Input-Remapper 2.0 - egér/bill. gomb konfiguráló, macro író program
+yay -S input-remapper-bin --noconfirm
+
+#QEMU - VM alkalmazás
+sudo pacman -S qemu-full qemu-img libvirt virt-install virt-manager virt-viewer edk2-ovmf dnsmasq swtpm guestfs-tools libosinfo tuned dmidecode --noconfirm
+sudo usermod -aG kvm $USER
+sudo usermod -aG input $USER
+sudo usermod -aG libvirt $USER
+sudo sed -i '530d' /etc/libvirt/qemu.conf
+sudo sed -i "530 i user = \"$USER"\" /etc/libvirt/qemu.conf
+sudo sed -i 's/#group = "kvm"/group = "kvm"/' /etc/libvirt/qemu.conf
+
+#Onlyoffice - (MS Office 365 alternative)
+sudo pacman -S onlyoffice-bin --noconfirm
+
+#Pavucontrol - Apponkénti hangerőszabályzás/konfigurálás
+
+sudo pacman -S pavucontrol --noconfirm
+
+#VLC - Videólejátszó
+flatpak install org.videolan.VLC --user -y
+
+#qBittorent - nyílt forráskódú torrent kliens
+sudo pacman -S qbittorrent --noconfirm
+cp -a qBittorrent /home/$USER/.config/
+
+#Parabolic - Videóletöltö kliens
+flatpak install flathub org.nickvision.tubeconverter --user -y
+
+#Vibrant - szín beállitó kliens
+flatpak install flathub io.github.libvibrant.vibrantLinux --user -y
+
+#Double Commander - Total Commander alternative
+sudo pacman -S doublecmd-gtk2 --noconfirm
+
+#Monophony - hang kiszedő
+yay -S monophony --noconfirm
+
