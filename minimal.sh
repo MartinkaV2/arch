64c75d445b9c855cd4906363202f931eb8c5e6e4
MartinkaV2 <70255079+MartinkaV2@users.noreply.github.com>
2025-01-13 14:31:52 +0100

    a

--- minimal.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-sudo pacman -S xfce4 xfce4-goodies lightdm lighdm-gtk-greeter gvfs nano openssh pavucontrol wget vim wireless_tools wpa_supplicant xarchiver xdg-utils iwd  --noconfirm
-
-sudo systemctl enable lightdm
