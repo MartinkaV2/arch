a7b581dc3684b87e4163df2dc7bbe33f205f3e4a
MartinkaV2 <70255079+MartinkaV2@users.noreply.github.com>
2025-01-13 08:37:06 +0100

    minimál telepítés béta 0.2

--- minimal.sh
+++ minimal.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sudo pacman -S xfce4 xfce4-goodies lightdm lighdm-gtk-greeter gvfs pavucontrol xarchiver --noconfirm
+sudo pacman -S xfce4 xfce4-goodies lightdm lighdm-gtk-greeter gvfs nano openssh pavucontrol wget vim wireless_tools wpa_supplicant xarchiver xdg-utils iwd  --noconfirm
 
 sudo systemctl enable lightdm
